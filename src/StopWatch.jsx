96c2c96b217e308b65026cd0c5a305bb250cbd62
Nitish <nitishbabubolla9966@gmail.com>
2024-06-18 19:44:55 +0530

    19-06-24

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useRef, useState } from "react";
 
 
 function StopWatch(){
-    return(
-        <div>
 
+    const [isRunning,setisRunning]=useState(false)
+    const [elapseTime,setElapseTime]=useState(0)
+
+    useEffect(()=>{
+
+    },[isRunning])
+
+    const intervalTime=useRef()
+
+    const startTime=useRef()
+
+    function start(){
+
+    }
+    function stop(){
+
+    }
+    function reset(){
+
+    }
+    function formate(){
+        return `00:00:00`
+    }
+
+    return(
+        <div className="container">
+            <div className="timer">{formate()}</div>
+            <div className="controller">
+                <button>Start</button>
+                <button>Reset</button>
+                <button>Stop</button>
+            </div>
         </div>
     );
 }
